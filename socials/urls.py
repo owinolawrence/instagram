bf514c25e7e09bcbac77a8eedfa6ece5d92b05c9
owinolawrence <owinolawrence21@gmail.com>
2020-01-06 13:39:18 +0300

    add a new urlpattern that references the location of uploade file

--- socials/urls.py
+++ socials/urls.py
@@ -1,11 +1,24 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import url 
 from .import views
 
+from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     
-    url('^$' , views.index , name = 'index'),
-    url(r'^search/', views.search_results, name='search_results')
+    
+    url('', views.signup, name='signup'),
+    url(r'^home/', views.home, name = 'home'),
+    url(r'^login/', auth_views.LoginView.as_view(template_name = 'auth/login.html'), name = 'login'),
+    url(r'^logout/', auth_views.LogoutView.as_view(template_name='auth/thankyou.html'), name = 'logout'),
+    url(r'^profile/', views.profile , name = 'my_profile'),
+    url(r'^image/new', views.createimage.as_view(), name = 'post'),
+    url(r'^comment/(?P<image_id>\d+)$',views.comments,name='comments'),
+    url(r'^like/$',views.like_post,name='like_post'),
+    url(r'^delete/(?P<image_id>\d+)$',views.delete,name='delete'),
+    url(r'^update/',views.update_profile,name='update_profile'),
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
